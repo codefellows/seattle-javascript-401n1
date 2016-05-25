70c4132f41811b0d75289572b26600c9c5955919
Duncan Marsh <dwolfm@gmail.com>
2016-05-24 23:16:31 -0700

    added lecture 11 code
    * express app
    * express router
    * simple storage module
    * storage module testing
    * AppError
    * note model
    * note router
    * note router testing

---
+++ lecture-11-express-router/lecture/express-note-api-demo/test/note-router-test.js
@@ -0,0 +1,81 @@
+'use strict';
+
+// set env varibales
+// require node modules
+// require npm modules
+const expect = require('chai').expect;
+const request = require('superagent'); 
+// require app modules
+const server = require('../server');
+const storage = require('../lib/storage');
+const Note = require('../model/note');
+// setup globals add require modules dependent on gloals
+const port = process.env.PORT || 3000;
+const baseUrl = `localhost:${port}/api/note`;
+
+describe('testing module note-router', function(){
+  before((done) => {
+    if (!server.isRunning){
+      server.listen(port, () => {
+        server.isRunning = true;
+        console.log('server running on port', port);
+        done();
+      });
+      return;
+    };
+    done();
+  });
+
+  after((done) => {
+    if (server.isRunning){
+      server.close(() => {
+        console.log('shutdown the server');
+        done();
+      });
+      return;
+    }
+    done();
+  });
+
+  describe('testing POST /api/note', function(){
+    after((done) => {
+      storage.pool = {};
+      done();
+    });
+    
+    it('should return a note', function(done){
+      request.post(baseUrl)
+      .send({content: 'test note'})
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.content).to.equal('test note');
+        expect(!!res.body.id);
+        done();
+      });
+    });
+  });
+
+  describe('testing GET /api/note', function(done){
+    before((done) => {
+      this.tempNote = new Note('test data');
+      storage.pool.note = {};
+      storage.pool.note[this.tempNote.id] = this.tempNote;
+      done();
+    });
+
+    after((done) => {
+      storage.pool = {};
+      done();
+    });
+
+    it('should return a note', (done) => {
+      request.get(`${baseUrl}/${this.tempNote.id}`)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.content).to.equal(this.tempNote.content);
+        expect(res.body.id).to.equal(this.tempNote.id);
+        done();
+      });
+    });
+  });
+});
