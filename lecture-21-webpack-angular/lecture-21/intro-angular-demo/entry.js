26f9902eca033295c1df369a2fc0236b3f98e459
Duncan Marsh <dwolfm@gmail.com>
2016-07-07 21:54:52 -0700

    added lecture 21

---
+++ lecture-21-webpack-angular/lecture-21/intro-angular-demo/entry.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// webpack assets
+require('!!file?name=[name].[ext]!./html/index.html');
+// npm modules
+const app = require('angular');
+// app modules
+// app logic
+
+app.module('demoApp', []);
