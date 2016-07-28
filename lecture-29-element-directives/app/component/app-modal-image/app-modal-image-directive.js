90dbe9defa5328e28a2fe93ce00e2e28af11fa15
Duncan Marsh <dwolfm@gmail.com>
2016-07-28 13:04:40 -0700

    lecture-29 code

---
+++ lecture-29-element-directives/app/component/app-modal-image/app-modal-image-directive.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const angular = require('angular')
+angular.module('demoApp')
+.directive('appModalImage', function(){
+  return {
+    restrict: 'E',
+    replace: true,
+    template: require('./app-modal-image.html'),
+    scope: {
+      closeModal: '&',
+    }
+  }
+})
